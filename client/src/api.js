8a6e7d8441837044f3db8d724cd9401075e26d1e
Arman Tastan <armantastan@MacBook-Air-Arman.local>
2026-01-28 15:17:12 +0500

    fix cors

--- client/src/api.js
+++ client/src/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5050/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 })
+
 
 export const attachRole = (role) => ({
   headers: {
